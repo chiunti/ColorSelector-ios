91cada731da4262307f26d73d1eaf774e5fe1011
Chiunti <chiunti.abc@gmail.com>
2015-01-19 22:00:32 -0600

    Initial Commit

---
+++ ColorSelTests/ColorSelTests.m
@@ -0,0 +1,40 @@
+//
+//  ColorSelTests.m
+//  ColorSelTests
+//
+//  Created by Chiunti on 19/01/15.
+//  Copyright (c) 2015 Chiunti. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface ColorSelTests : XCTestCase
+
+@end
+
+@implementation ColorSelTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
